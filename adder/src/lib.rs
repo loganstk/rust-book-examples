1e434df82578ae3252cf5c5bb3b2957c8c45bfab
Andrii Lohvynchuk <logan.zhi@gmail.com>
2024-06-13 15:02:19 +0300

    Add sample docs

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,5 +1,24 @@
+//! # My Crate
+//!
+//! `my_crate` is a collection of utilities to make performing certain
+//! calculations more convenient.
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
 }
 
 pub fn add_two(a: i32) -> i32 {
